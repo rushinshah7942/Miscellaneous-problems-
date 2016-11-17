bb38522af57ee2b7435af3ec1b23e8edf24040e2
rushinshah111 <rushinshah7942@gmail.com>
2016-11-17 17:38:21 -0500

    Combine Anagrams together..two methods

--- CombineAnagramWordsTogether.java
+++ CombineAnagramWordsTogether.java
@@ -60,3 +60,37 @@ public class PrintAnagramsTogether
 		return result;
 	}
 }
+
+
+// Another solution
+
+public List<List<String>> groupAnagrams(String[] strs) {
+    List<List<String>> result = new ArrayList<List<String>>();
+ 
+    HashMap<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
+    for(String str: strs){
+		
+        // below is the main logic
+		// If two strings are anagram to each other, their sorted sequence is the same.
+		char[] arr = new char[26];
+        for(int i=0; i<str.length(); i++){
+            arr[str.charAt(i)-'a']++;
+        }
+        String ns = new String(arr);
+ 
+        if(map.containsKey(ns)){
+            map.get(ns).add(str);
+        }else{
+            ArrayList<String> al = new ArrayList<String>();
+            al.add(str);
+            map.put(ns, al);
+        }
+    }
+ 
+    result.addAll(map.values());
+ 
+    return result;
+}
+Time Complexity
+
+If the average length of verbs is m and array length is n, then the time is O(n*m).
