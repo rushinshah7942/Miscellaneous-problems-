59971d71a36412aa6755be85f3f63530d9e6b5d5
rushinshah111 <rushinshah7942@gmail.com>
2016-12-05 00:17:30 -0500

    changes

--- BestTimetoBuyandSellStock_III.java
+++ BestTimetoBuyandSellStock_III.java
@@ -7,3 +7,22 @@ Note:
 You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
 */
 
+public class Solution 
+{
+    public int maxProfit(int[] prices) 
+    {
+        int maxProfit1 = 0; 
+	    int maxProfit2 = 0; 
+	    int lowestBuyPrice1 = Integer.MAX_VALUE;
+	    int lowestBuyPrice2 = Integer.MAX_VALUE;
+	   
+	    for(int p:prices)
+	    {
+	    	maxProfit2 = Math.max(maxProfit2, p-lowestBuyPrice2);
+	    	lowestBuyPrice2 = Math.min(lowestBuyPrice2, p-maxProfit1);
+	    	maxProfit1 = Math.max(maxProfit1, p-lowestBuyPrice1);
+	    	lowestBuyPrice1 = Math.min(lowestBuyPrice1, p);
+	    }
+	    return maxProfit2;        
+    }
+}
