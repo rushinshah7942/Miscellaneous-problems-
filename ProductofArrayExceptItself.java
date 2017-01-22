3bd077d0f0d022049277002c3bb70e512f40907f
rushinshah111 <rushinshah7942@gmail.com>
2017-01-21 22:33:15 -0800

    Amazon question - leetcode medium - Product of array without itself (without using divison operation)

---
+++ ProductofArrayExceptItself.java
@@ -0,0 +1,38 @@
+/*
+AMAZON INTERVIEW QUESTION
+---------------------------
+
+Given an array of n integers where n > 1, nums, return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+
+Solve it without division and in O(n).
+
+For example, given [1,2,3,4], return [24,12,8,6].
+
+Follow up:
+Could you solve it with constant space complexity? (Note: The output array does not count as extra space for the purpose of space complexity analysis.)
+
+*/
+
+/*
+
+nums    1 2 3 4 
+result  1 1 2 6   
+
+temp    1 4 12 24
+result  24 12 8 6
+
+*/
+
+// Use tmp to store temporary multiply result by two directions. Then fill it into result.
+public int[] productExceptSelf(int[] nums) {
+    int[] result = new int[nums.length];
+    for (int i = 0, tmp = 1; i < nums.length; i++) {
+        result[i] = tmp;
+        tmp *= nums[i];
+    }
+    for (int i = nums.length - 1, tmp = 1; i >= 0; i--) {
+        result[i] *= tmp;
+        tmp *= nums[i];
+    }
+    return result;
+}
