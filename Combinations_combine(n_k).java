fc7b5d7b09417eccdeed0e1366302516f99b6b25
rushinshah111 <rushinshah7942@gmail.com>
2017-01-29 15:13:49 -0800

    Leetcode Combination problem - hard

--- Combinations_combine(n_k).java
+++ Combinations_combine(n_k).java
@@ -124,3 +124,84 @@ m: from 5 to 5
 after adding elements: [[1, 2, 3, 4], [1, 2, 3, 5], [1, 2, 4, 5], [1, 3, 4, 5], [2, 3, 4, 5]]
 
 */
+
+// backtracking solution
+
+// 1. the valid range for the 1st position is [1, n-k+1]
+// 2. the valid range for the i-th position is [number we selected for previous position+1, n-k+1+i]
+
+public class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> ans = new ArrayList<>();
+        dfs(ans, new ArrayList<Integer>(), k, 1, n-k+1); // point 1
+        return ans;
+    }
+
+    private void dfs(List<List<Integer>> ans, List<Integer> list, int kLeft, int from, int to) {
+        if (kLeft == 0) { 
+			ans.add(new ArrayList<Integer>(list)); return; 
+		}
+        for (int i=from; i<=to; ++i) {
+            list.add(i);
+            dfs(ans, list, kLeft-1, i+1, to+1); // point 2
+            list.remove(list.size()-1);
+        }
+    }
+}
+
+/*
+
+Execution Flow:
+
+====
+
+ans: [] list:[]
+adding from:1 to:2
+----------
+====
+
+ans: [] list:[1]
+adding from:2 to:3
+--------------------
+====
+
+ans: [] list:[1, 2]
+adding from:3 to:4
+------------------------------
+====
+
+ans: [] list:[1, 2, 3]
+adding from:4 to:5
+--------------------------------------------------------------------------------------------------------------
+====
+
+ans: [[1, 2, 3, 4], [1, 2, 3, 5]] list:[1, 2, 4]
+adding from:5 to:5
+------------------------------------------------------------
+====
+
+ans: [[1, 2, 3, 4], [1, 2, 3, 5], [1, 2, 4, 5]] list:[1, 3]
+adding from:4 to:4
+------------------------------
+====
+
+ans: [[1, 2, 3, 4], [1, 2, 3, 5], [1, 2, 4, 5]] list:[1, 3, 4]
+adding from:5 to:5
+--------------------------------------------------
+====
+
+ans: [[1, 2, 3, 4], [1, 2, 3, 5], [1, 2, 4, 5], [1, 3, 4, 5]] list:[2]
+adding from:3 to:3
+--------------------
+====
+
+ans: [[1, 2, 3, 4], [1, 2, 3, 5], [1, 2, 4, 5], [1, 3, 4, 5]] list:[2, 3]
+adding from:4 to:4
+------------------------------
+====
+
+ans: [[1, 2, 3, 4], [1, 2, 3, 5], [1, 2, 4, 5], [1, 3, 4, 5]] list:[2, 3, 4]
+adding from:5 to:5
+----------------------------------------
+
+*/
